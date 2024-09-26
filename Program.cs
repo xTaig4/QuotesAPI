54a41e48371264da9705f9373969eb0a110ed46a
TaiStudent <xTaig4@gmail.com>
2024-09-26 08:38:38 +0200

    Added ratelimiting sliding window policy

--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.EntityFrameworkCore;
+using QuoteAPI;
 using QuoteAPI.Data;
 using System.Threading.RateLimiting;
 
 
 var builder = WebApplication.CreateBuilder(args);
+var myOptions = new RateLimitSettings();
 
 // Add services to the container.
 builder.Services.AddDbContext<QuoteContext>();
+builder.Configuration.GetSection(RateLimitSettings.MyRateLimit).Bind(myOptions);
 
 builder.Services.AddRateLimiter(_ => _.AddFixedWindowLimiter(policyName: "fixed", options =>{
     options.PermitLimit = 4; 
@@ -16,6 +19,12 @@ builder.Services.AddRateLimiter(_ => _.AddFixedWindowLimiter(policyName: "fixed"
     options.QueueLimit = 2; 
 }));
 
+builder.Services.AddRateLimiter(_ => _.AddSlidingWindowLimiter(policyName: "sliding", options => { 
+    options.PermitLimit = myOptions.PermitLimit; 
+    options.Window = TimeSpan.FromSeconds(myOptions.Window); 
+    options.SegmentsPerWindow = myOptions.SegmentsPerWindow; 
+    options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst; 
+    options.QueueLimit = myOptions.QueueLimit; }));
 
 builder.Services.AddCors(options =>
 {
---
+++ RateLimitSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace QuoteAPI
+{
+    public class RateLimitSettings
+    {
+        public const string MyRateLimit = "RatelimitSettings";
+        public int PermitLimit { get; set; }
+        public int SegmentsPerWindow { get; set; }
+        public int Window { get; set; }
+        public int QueueLimit { get; set; }
+    }
+}
